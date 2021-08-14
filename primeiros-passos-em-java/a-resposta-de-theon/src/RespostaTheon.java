e70df5c6a06b70cf18efb258d0da4d26195e00a2
vitor-mda <vitor-mda@hotmail.com>
2021-08-13 23:07:10 -0300

    conserta indentação

--- primeiros-passos-em-java/a-resposta-de-theon/src/RespostaTheon.java
+++ primeiros-passos-em-java/a-resposta-de-theon/src/RespostaTheon.java
@@ -3,23 +3,23 @@ import java.util.Scanner;
 
 public class RespostaTheon {
 
-public static void main(String[] args) throws IOException {
-        Scanner leitor = new Scanner(System.in);
-        int N = leitor.nextInt();
-        int T, menor = 0, posMenor = 0;
+        public static void main(String[] args) throws IOException {
+                Scanner leitor = new Scanner(System.in);
+                int N = leitor.nextInt();
+                int T, menor = 0, posMenor = 0;
 
-        for(int pos = 1; pos <= N; pos++) {
-                T = leitor.nextInt();
+                for(int pos = 1; pos <= N; pos++) {
+                        T = leitor.nextInt();
 
-                if(T < menor) {
-                        menor = T;
-                        posMenor = pos;
-                } else if(pos == 1) {
-                        menor = T;
-                        posMenor = pos;
+                        if(T < menor) {
+                                menor = T;
+                                posMenor = pos;
+                        } else if(pos == 1) {
+                                menor = T;
+                                posMenor = pos;
+                        }
+                }
+
+                System.out.println(posMenor);
                 }
         }
-
-        System.out.println(posMenor);
-        }
-}
